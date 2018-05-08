14bcbdc0dc2890c9a4ed3d9405f47ad550d6635f
johnnybuma <johnny.buma@gmail.com>
2018-05-08 08:27:48 -0700

    Fuck unpermitted params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -71,6 +71,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :subtitle, :slug, :body, :keyword)
+      params.require(:article).permit(:title, :subtitle, :slug, :body, :keyword, :image)
     end
 end
