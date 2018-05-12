98422fee7aa9aca5de4a14a62f77912338e762e8
johnnybuma <johnny.buma@gmail.com>
2018-05-11 19:10:42 -0700

    Fixed set slug error in create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,8 +39,6 @@ class ArticlesController < ApplicationController
 
     @article.user = current_user
 
-    @article.slug = to_slug(@article.slug)
-
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
